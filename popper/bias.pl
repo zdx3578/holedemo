edeb7464f3828d16da983ffb09a31de7db1dbfa6
zdx <179363811@qq.com>
2025-06-16 08:32:15 +0800

    bug run

--- popper/bias.pl
+++ popper/bias.pl
@@ -1,43 +1,28 @@
 %%%%%%%%%%%%  Predicate catalogue  %%%%%%%%%%%%
 
 % head predicate
-head_pred(transform,2).
+%%%%%%%%%%%%  搜索空间  %%%%%%%%%%%%
 
-body_pred(color,2).
-body_pred(holes,2).
-body_pred(has_hole,1).
-body_pred(object,1).
+head_pred(transform,2).            % transform(+Obj,+Color)
 
-type(obj).
-type(color).
-type(int).
+body_pred(color,2).                % color(+Obj,+Color)
+body_pred(holes,2).                % holes(+Obj,+Int)
+body_pred(has_hole,1).             % has_hole(+Obj)
+body_pred(target_color,1).         % <— 新增一元常量谓词
+
+%%%%%%%%%%%%  类型  %%%%%%%%%%%%
+
+type(obj).     type(color).     type(int).
 
 type(transform,(obj,color)).
 type(color,    (obj,color)).
 type(holes,    (obj,int)).
 type(has_hole, (obj,)).
-type(object,   (obj,)).
+type(target_color,(color)).      % <— 类型只有一个 color
 
 constant(color,4).
 
-direction(transform/2,(in,out)).
-direction(color/2,(in,out)).
-direction(holes/2,(in,out)).
-direction(has_hole/1,(in)).
-direction(object/1,(in)).
+%%%%%%%%%%%%  限制  %%%%%%%%%%%%
 
+max_body(2).   max_vars(3).   max_clauses(1).
 
-body_pred(my_geq,2).      type(my_geq,(int,int)).
-body_pred(my_leq,2).      type(my_leq,(int,int)).
-body_pred(my_gt,2).       type(my_gt,(int,int)).
-body_pred(my_lt,2).       type(my_lt,(int,int)).
-body_pred(my_add,3).      type(my_add,(int,int,int)).
-body_pred(my_mult,3).     type(my_mult,(int,int,int)).
-body_pred(my_subtract,3). type(my_subtract,(int,int,int)).
-
-constant(int,0).          % 至少要有一个 int 常量
-
-max_body(2).
-max_vars(3).
-max_clauses(1).
-non_datalog.
